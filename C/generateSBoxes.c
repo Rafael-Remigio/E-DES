228a53198c5606561333b561180204ad9a14a9d9
rafael <rafael.lixa@ua.pt>
2023-11-01 11:39:17 +0000

    Update vector generation

--- C/generateSBoxes.c
+++ C/generateSBoxes.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
-
-
+#include <openssl/sha.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Swap Function to help with the Shufling algorithm
 void swap(uint8_t *a, uint8_t *b) {
@@ -14,7 +15,7 @@ void swap(uint8_t *a, uint8_t *b) {
 // Fisher Yates Algorithm
 // It allows us to shuffle with a deterministic vector
 // It also has linear time complexity, and O(1) space complexity.
-void fisherYates ( uint8_t ∗ array , int* vector )
+void fisherYates ( uint8_t *array , int* vector )
 {
 	int i , j ;
 	int swapper = 0;
@@ -22,14 +23,46 @@ void fisherYates ( uint8_t ∗ array , int* vector )
 		// Get next value to be swapped, Fisher Yates algorithm
 		swapper = vector[i] % (256 -i);
 		// Swap last value with value on another position
-		swap(array[swapper] , array[256-i]);
+		swap(&array[swapper], &array[256-i]);
 	}
 }
 
 // Generate vector from Key
 // This vector will be used to shuffle the S-Boxes in a deterministic way
-void generateShuffleVector()
+int *generateShuffleVector(char *key)
 {
+	// Key length is 32 bytes
+	int key_length = 32;
+	// Declare key to be incremented
+	char key_with_index[34];
+	// Hash value on each iteration
+	char* hash;
 
+	// Allocate memory
+	hash = malloc(SHA256_DIGEST_LENGTH * sizeof(char));
+
+
+	for (int i = 0; i < 4 ; i ++){
+
+
+		sprintf(key_with_index, "%.32s%d", key, i);
+		printf("%.33s\n", key_with_index);
+		SHA256(key_with_index, 33, hash);
+
+		printf("Hash: %.32s\n",hash);
+	}
 }
 
+
+
+
+int main(int argc, char const *argv[])
+{
+
+	char key[32] = "aaaaaaaaabbbbbbbbbbcccccdddddddd";
+
+	printf("%.32s\n", key);
+
+
+	generateShuffleVector(key);
+}
